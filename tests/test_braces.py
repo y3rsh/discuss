d633b7825b879c17278ae29f6465781f99a1007b
Josh McVey <yersh13@gmail.com>
2019-10-07 09:24:37 -0500

    fix: better conversation

---
+++ tests/test_braces.py
@@ -0,0 +1,39 @@
+import pytest
+
+
+def fail_fast(expression):
+    if not expression or len(expression) % 2 != 0:
+        return True
+
+
+def is_matched(expression):
+    if fail_fast(expression):
+        return False
+    mapping = {"(": ")", "{": "}", "[": "]"}
+    queue = []
+    for letter in expression:
+        if letter in mapping:
+            queue.append(mapping[letter])
+        elif not (queue and letter == queue.pop()):
+            return False
+    return not queue
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "input, output",
+    [
+        ("]{}", False),
+        ("{(})", False),
+        ("{()}", True),
+        ("[({({{{}}})})]", True),
+        (None, False),
+        ("", False),
+    ],
+)
+def test_matching_braces(input, output):
+    assert is_matched(input) == output
+
+
+mapping = dict(zip("({[", ")}]"))
+mapping = {"(": ")", "{": "}", "[": "]"}
